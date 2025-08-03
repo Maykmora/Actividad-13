46bd1f76e2a7c3b149dbfc73f96049ad1c803318
Maykmora <maykmora12@gmail.com>
2025-08-02 23:03:50 -0600

    Commit 1

---
+++ ejercicio_01.py
@@ -0,0 +1,12 @@
+def menu():
+    print("1.Agregar estudiante")
+    print("2.Agregar curso con nota")
+    print("3.Consultar estudiante")
+    print("4.Calcular promedio de estudiante")
+    print("5.Verificar si aprueba")
+    print("6.Mostramos todos los estudiantes")
+    print("7.Salir")
+
+
+while True:
+    menu()
